1d96dff5e1e2b5e36d3fcfa02ad40d92be5204d7
Dikshant <dikkpsd@gmail.com>
2024-02-13 16:14:28 +0530

    Added first animation

---
+++ lib/animations/axis_rotation.dart
@@ -0,0 +1,61 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+class AxisRotation extends StatefulWidget {
+  const AxisRotation({super.key});
+
+  @override
+  State<AxisRotation> createState() => _AxisRotationState();
+}
+
+class _AxisRotationState extends State<AxisRotation>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation<double> _animation;
+  @override
+  void initState() {
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+    _controller.repeat();
+    _animation = Tween(begin: 0.0, end: 2 * pi)
+        .animate(_controller); // Tween is like BeTween
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white.withOpacity(0.4),
+      body: Center(
+        // animated Builder used to inform about rerendering of the widget
+        child: AnimatedBuilder(
+            // animated Builder applied mostly to transforms
+            animation: _controller,
+            builder: (context, child) {
+              return Transform(
+                alignment: Alignment.center, // rotation along alignment center
+                // origin: const Offset(50, 50), // rotation origin (counted from topLeft of the container)
+                transform: Matrix4.identity()
+                  ..rotateY(_animation
+                      .value), // basically rotate works in place on matrix
+                child: Container(
+                    width: 50,
+                    height: 50,
+                    decoration: const BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    )),
+              );
+            }),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animations/animations/axis_rotation.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Animations',
+      theme: ThemeData(brightness: Brightness.dark),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      themeMode: ThemeMode.dark,
+      debugShowCheckedModeBanner: false ,
+      debugShowMaterialGrid: false,
+      home: const AxisRotation(),
+    );
+  }
+}
+
